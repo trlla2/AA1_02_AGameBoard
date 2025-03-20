e596c598fbf6753c0f4991538b53ef21eceed4e1
trlla2 <116338442+trlla2@users.noreply.github.com>
2025-03-20 09:21:48 +0100

    vector deleted

--- GameBoard/GameBoard/Board.cpp
+++ GameBoard/GameBoard/Board.cpp
@@ -24,75 +24,69 @@ void Board:: initialieBoard(Player* player) {
 	}
 
 	//give each box a value
+	int emptyBoxesSize = 0;
 
-	vector<pair<int, int>> emptyBoxes; //Vector with all the empty Boxes
 
 	// fill array
 	for (int i = 0; i < dynamicSizeX; i++) {
 		for (int j = 0; j < dynamicSizeY; j++) {
 			// Verify if is wall
 			if (i == 0 || i == dynamicSizeX - 1 || j == 0 || j == dynamicSizeY - 1) {
-				box[i][j] = 'W';
+				box[i][j] = 'W'; // Wall
 			}
 			else {
-				box[i][j] = '.';
+				box[i][j] = '.'; // Empty box
 
-				emptyBoxes.push_back(make_pair(i, j)); // push empty positions to vector
+				emptyBoxesSize++;
 			}
 		}
 	}
 
-	if (!emptyBoxes.empty()) { // check if there is empty boxes
-
-		int maxSpikes =  emptyBoxes.size() * 0.1; // 10%  of spikes
-		int maxGems = emptyBoxes.size() * 0.25; // 25% of gems
-		
-
-		if (maxSpikes > 0) {
-			numSpikes = GenerateClampedRandom(0, maxSpikes); // generate num of Spikes
-		}
-		else
-		{
-			numSpikes = 1; // Forced to atlest have 1 spike
-		}
-
-		if (maxGems > 0) {
-			numGems = GenerateClampedRandom(0, maxGems); // generate num of Gems
-		}
-		else
-		{
-			numGems = 1; // Forced to atlest have 1 gem
-		}
-		
-		int leftSpikes = numSpikes; // contains num of left spikes to print
-		int leftGems = numGems; // contains num of left gems to print
+	int maxSpikes = emptyBoxesSize * 0.1; // 10%  of spikes
+	int maxGems = emptyBoxesSize * 0.25; // 25% of gems
 
 
-		std::random_shuffle(emptyBoxes.begin(), emptyBoxes.end()); // shufle empty Boxes
+	if (maxSpikes > 0) {
+		numSpikes = GenerateClampedRandom(0, maxSpikes); // generate num of Spikes
+	}
+	else
+	{
+		numSpikes = 1; // Forced to atlest have 1 spike
+	}
 
-		for (int i = emptyBoxes.size() - 1; i >= 0; i--) { // spawn spikes and gems on emptyBoxes
+	if (maxGems > 0) {
+		numGems = GenerateClampedRandom(0, maxGems); // generate num of Gems
+	}
+	else
+	{
+		numGems = 1; // Forced to atlest have 1 gem
+	}
+
+	int leftSpikes = numSpikes; // contains num of left spikes to print
+	int leftGems = numGems; // contains num of left gems to print
+
+	bool playerSpawned = false; // check if player is Spawned
+
+	while ((leftSpikes > 0 || leftGems > 0 || !playerSpawned)) {
+		int randomXPos = GenerateClampedRandom(0, dynamicSizeX - 1); // Get random position from boad
+		int randomYPos = GenerateClampedRandom(0, dynamicSizeY - 1);
+
+		if (box[randomXPos][randomYPos] == '.'){ // if is empty 
 			if (leftSpikes > 0) { //Spawn all spikes
 				leftSpikes--;
-				box[emptyBoxes[i].first][emptyBoxes[i].second] = 'S'; // Spike
-				emptyBoxes.pop_back(); // delete that empty box from vector
+				box[randomXPos][randomYPos] = 'S'; // Spike
 			}
 			else if (leftGems > 0) { //Spawn all Gems
 				leftGems--;
-				box[emptyBoxes[i].first][emptyBoxes[i].second] = 'G'; // Gem
-				emptyBoxes.pop_back(); // delete that empty box from vector
+				box[randomXPos][randomYPos] = 'G'; // Gem
 			}
 			else { //Spawn Player
-				box[emptyBoxes[i].first][emptyBoxes[i].second] = 'P'; // Player
-				player->setPosition(emptyBoxes[i].first, emptyBoxes[i].second);
-				emptyBoxes.pop_back(); // delete that empty box from vector
-				break;
+				box[randomXPos][randomYPos] = 'P'; // Player
+				player->setPosition(randomXPos, randomYPos);
+				playerSpawned = true;
 			}
-			
 		}
 	}
-
-
-
 
 }
 
